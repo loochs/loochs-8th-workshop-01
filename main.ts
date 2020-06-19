5fc4496c17183b89aeeed45b19f8f3c8ae3ba6d6
Loochs <54494160+loochs@users.noreply.github.com>
2020-06-19 22:13:35 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
-input.onButtonPressed(Button.A, function () {
-    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
-})
+let カードが開いた = 0
 basic.forever(function () {
-    for (let index = 0; index < 4; index++) {
-        basic.showIcon(IconNames.SmallHeart)
-        basic.showIcon(IconNames.Heart)
+    if (input.lightLevel() > 180 && カードが開いた == 0) {
+        カードが開いた = 1
+        music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+        for (let index = 0; index < 8; index++) {
+            basic.showIcon(IconNames.SmallHeart)
+            basic.showIcon(IconNames.Heart)
+        }
+    } else if (input.lightLevel() <= 20) {
+        カードが開いた = 0
+        basic.clearScreen()
+        music.stopMelody(MelodyStopOptions.All)
     }
 })
